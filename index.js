53c167ba13dff18c9fe454979dd9ab0b58220bde
RobsonTT <robson.google@hotmail.com>
2021-02-26 21:28:33 -0300

    Function
    I created a basic example of a function to run in the google cloud

---
+++ index.js
@@ -0,0 +1,14 @@
+// [START functions_helloworld_get]
+/**
+ * HTTP Cloud Function.
+ * This function is exported by index.js, and is executed when
+ * you make an HTTP request to the deployed function's endpoint.
+ *
+ * @param {Object} req Cloud Function request context.
+ *                     More info: https://expressjs.com/en/api.html#req
+ * @param {Object} res Cloud Function response context.
+ *                     More info: https://expressjs.com/en/api.html#res
+ */
+exports.helloGET = (req, res) => {
+  res.send('HTTP function successful execution =)');
+};
